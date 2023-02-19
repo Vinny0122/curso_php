5883d08422a9f7083a250f1b1e593498fbb38702
Vinícius <nicius30@gmail.com>
2023-02-19 19:53:38 -0300

    Pasta 1_case_sensitive

---
+++ 2_conceitos_basicos/1_case_sensitive/index.php
@@ -0,0 +1,14 @@
+<?php
+    //Não é case sensitive
+    echo "Teste <br>";
+    ecHO "Teste2 <br>";
+    ECHO "Teste3 <br>";
+    //Não é case sensitive
+    $nome = "Vinícius";
+    $NOME = "Teste";
+    
+    echo "<br>";
+    echo $NOME;
+    echo "<br>";
+    echo $nome;
+?>
