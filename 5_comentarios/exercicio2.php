effc84204a2c75896112dca3c66e8a126ee49027
Vinicius <nicius30@gmail.com>
2023-02-24 00:27:58 -0300

    Aulas 23/02

---
+++ 5_comentarios/exercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+    #Objeto
+    $obj = "Notebook Dell G7";
+    #Capacidade
+    $memoria = "32 GB de memória ram";
+    #Espaço
+    $armazenamento = "3 TB de armazenamento";
+
+    echo $obj,  "<br>"; //Primeira linha
+    echo $memoria, "<br>"; //Segunda linha
+    echo $armazenamento, "<br>"; //Terceira linha
